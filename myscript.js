931e74513768509ac2df310d91de150709894e47
anicalymus <anicalymus@gmail.com>
2018-07-30 18:58:04 -0700

    adding js, css and html

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var announcement = "Next seminar is August 18th!";
+var greeting = "Hello!";
+
+function announce(){
+	document.getElementById('main').innerHTML = announcement;
+};
+
+announce(announcement);
